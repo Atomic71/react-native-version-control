dc537c351755cc63ff04bdf424ea24c4301260a6
Atomic71 <tomicandrej71@gmail.com>
2023-11-22 09:59:08 +0100

    chore: add user nav

--- apps/web/src/components/user-nav.tsx
+++ apps/web/src/components/user-nav.tsx
@@ -3,7 +3,11 @@ import Link from 'next/link';
 import { type ReactElement } from 'react';
 
 function UserNav(): ReactElement {
-  return <Link href='/auth/signout'>Logout</Link>;
+  return (
+    <nav>
+      <Link href='/auth/signout'>Logout</Link>
+    </nav>
+  );
 }
 
 export default UserNav;
