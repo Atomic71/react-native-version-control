16bd52bb038995e1e8f9f497dfa1cc026da95778
Atomic71 <tomicandrej71@gmail.com>
2023-11-22 14:23:48 +0100

    feat: add redirect on login

--- apps/web/src/components/user-auth.tsx
+++ apps/web/src/components/user-auth.tsx
@@ -2,7 +2,8 @@
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { createBrowserClient } from '@supabase/ssr';
-import { type ReactElement } from 'react';
+import { useEffect, type ReactElement } from 'react';
+import { useRouter } from 'next/navigation';
 import { env } from '../env.mjs';
 
 const client = createBrowserClient(
@@ -11,12 +12,25 @@ const client = createBrowserClient(
 );
 
 function UserAuth(): ReactElement {
+  const router = useRouter();
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = client.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_IN') {
+        router.replace('/dashboard');
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [router]);
   return (
     <Auth
       appearance={{ theme: ThemeSupa }}
       dark
       providers={[]}
-      redirectTo='/dashboard'
+      redirectTo='dashboard'
       supabaseClient={client}
       view='sign_in'
     />
