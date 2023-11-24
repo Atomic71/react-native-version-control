d1e27962cd79673aeedf441516da50e032256380
Atomic71 <tomicandrej71@gmail.com>
2023-11-24 08:48:17 +0100

    chore: update db types

--- packages/db/src/index.types.ts
+++ packages/db/src/index.types.ts
@@ -4,41 +4,58 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       App: {
         Row: {
-          created_at: string;
-          id: number;
-          name: string | null;
-        };
+          created_at: string
+          id: number
+          last_supported_android_version: string | null
+          last_supported_ios_version: string | null
+          name: string | null
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          id?: number;
-          name?: string | null;
-        };
+          created_at?: string
+          id?: number
+          last_supported_android_version?: string | null
+          last_supported_ios_version?: string | null
+          name?: string | null
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          id?: number;
-          name?: string | null;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string
+          id?: number
+          last_supported_android_version?: string | null
+          last_supported_ios_version?: string | null
+          name?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "App_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      AppOS: 'iOS' | 'Android';
-    };
+      AppOS: "iOS" | "Android"
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
