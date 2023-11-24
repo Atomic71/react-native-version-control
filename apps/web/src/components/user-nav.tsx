7a94c3990d9ef426e09026312b14f6ba77bacf30
Atomic71 <tomicandrej71@gmail.com>
2023-11-24 08:48:32 +0100

    chore: make nav look pretty

--- apps/web/src/components/user-nav.tsx
+++ apps/web/src/components/user-nav.tsx
@@ -4,7 +4,8 @@ import { type ReactElement } from 'react';
 
 function UserNav(): ReactElement {
   return (
-    <nav>
+    <nav className='px-10 py-4 justify-between flex bg-teal-300 text-gray-800'>
+      <h3>React Native Version Control</h3>
       <Link href='/auth/signout'>Logout</Link>
     </nav>
   );
