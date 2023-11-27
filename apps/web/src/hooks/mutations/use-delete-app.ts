5ecbcbfdcdebcc96ec7858d88226e103b0f538ec
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 10:50:22 +0100

    fix: rename fn

--- apps/web/src/hooks/mutations/use-delete-app.ts
+++ apps/web/src/hooks/mutations/use-delete-app.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { browserClient } from '../../db/supabase';
 
 //eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- inference is better
-export const useCreateApp = ({ onSuccess }: { onSuccess: () => void }) => {
+export const useDeleteApp = ({ onSuccess }: { onSuccess: () => void }) => {
   return useMutation({
     mutationFn: async (id: string) => {
       const data = await browserClient.from('apps').delete().eq('id', id);
