c621d86e3f89bc34d7c2196b3b661309fb7fb41d
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 09:25:23 +0100

    chore: refresh types

--- packages/db/src/index.types.ts
+++ packages/db/src/index.types.ts
@@ -9,28 +9,60 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      app_versions: {
+        Row: {
+          app: number | null
+          app_os: Database["public"]["Enums"]["AppOS"] | null
+          created_at: string
+          id: number
+          is_blocked: boolean | null
+          is_latest: boolean | null
+          version_number: string
+        }
+        Insert: {
+          app?: number | null
+          app_os?: Database["public"]["Enums"]["AppOS"] | null
+          created_at?: string
+          id?: number
+          is_blocked?: boolean | null
+          is_latest?: boolean | null
+          version_number: string
+        }
+        Update: {
+          app?: number | null
+          app_os?: Database["public"]["Enums"]["AppOS"] | null
+          created_at?: string
+          id?: number
+          is_blocked?: boolean | null
+          is_latest?: boolean | null
+          version_number?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "app_versions_app_fkey"
+            columns: ["app"]
+            isOneToOne: false
+            referencedRelation: "apps"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       apps: {
         Row: {
           created_at: string
           id: number
-          last_supported_android_version: string | null
-          last_supported_ios_version: string | null
           name: string | null
           user_id: string
         }
         Insert: {
           created_at?: string
           id?: number
-          last_supported_android_version?: string | null
-          last_supported_ios_version?: string | null
           name?: string | null
           user_id: string
         }
         Update: {
           created_at?: string
           id?: number
-          last_supported_android_version?: string | null
-          last_supported_ios_version?: string | null
           name?: string | null
           user_id?: string
         }
