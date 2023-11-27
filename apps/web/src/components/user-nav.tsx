21031120cebaadddbcc6e4388ab0f1681a1d824c
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 10:04:55 +0100

    feat: add e-mail address to navbar

--- apps/web/src/components/user-nav.tsx
+++ apps/web/src/components/user-nav.tsx
@@ -1,12 +1,19 @@
-'use client';
 import Link from 'next/link';
 import { type ReactElement } from 'react';
+import { cookies } from 'next/headers';
+import { makeServerClient } from '../db/supabase';
 
-function UserNav(): ReactElement {
+async function UserNav(): Promise<ReactElement> {
+  const client = makeServerClient(cookies());
+  const {
+    data: { user },
+  } = await client.auth.getUser();
   return (
     <nav className='px-10 py-4 justify-between flex bg-teal-300 text-gray-800'>
       <h3>React Native Version Control</h3>
-      <Link href='/auth/signout'>Logout</Link>
+      <div className='flex items-center gap-2'>
+        <p>{user?.email}</p>|<Link href='/auth/signout'>Logout</Link>
+      </div>
     </nav>
   );
 }
