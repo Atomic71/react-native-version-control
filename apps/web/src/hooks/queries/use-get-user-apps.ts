44b26177228e9d905e92bd177ae4d4fd0bae0854
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 13:34:40 +0100

    chore: add keys to index file

--- apps/web/src/hooks/queries/use-get-user-apps.ts
+++ apps/web/src/hooks/queries/use-get-user-apps.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { browserClient } from '../../db/supabase';
+import { keys } from './keys';
 
 //eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- inference is better
 export const useGetUserApps = () => {
   const query = useQuery({
-    queryKey: ['apps'],
+    queryKey: [keys.apps],
     queryFn: async () => {
       const data = await browserClient
         .from('apps')
