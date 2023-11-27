ec76bb6a6c08afb84bcf3fecec9038a63043015d
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 09:25:14 +0100

    chore: add types for clients

--- apps/web/src/db/supabase.ts
+++ apps/web/src/db/supabase.ts
@@ -1,7 +1,21 @@
-import { createBrowserClient } from '@supabase/ssr';
+import type { Database } from 'db';
+import type { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+import { createBrowserClient, createServerClient } from '@supabase/ssr';
 import { env } from '../env.mjs';
 
-export const browserClient = createBrowserClient(
+export const browserClient = createBrowserClient<Database>(
   env.NEXT_PUBLIC_SUPABASE_URL,
   env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+export const makeServerClient = (cookieStore: ReadonlyRequestCookies) =>
+  createServerClient<Database>(
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
