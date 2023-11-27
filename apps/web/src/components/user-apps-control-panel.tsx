2f48e2a039ec111ebf9c5a50f0989407944fb67b
Atomic71 <tomicandrej71@gmail.com>
2023-11-27 10:45:46 +0100

    feat: read/create app from dashboard

--- apps/web/src/components/user-apps-control-panel.tsx
+++ apps/web/src/components/user-apps-control-panel.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
+import { Button } from '@supabase/ui';
 import { browserClient } from '../db/supabase';
+import { useCreateApp } from '../hooks/mutations/use-create-app';
 
 export default function UserAppsControlPanel(): JSX.Element {
   const query = useQuery({
     queryKey: ['apps'],
     queryFn: async () => {
-      const data = await browserClient.from('apps').select();
+      const data = await browserClient
+        .from('apps')
+        .select('*', { count: 'exact' });
       return data;
     },
     staleTime: 1000 * 5 * 60,
@@ -15,11 +20,43 @@ export default function UserAppsControlPanel(): JSX.Element {
     refetchOnReconnect: false,
     refetchOnWindowFocus: false,
   });
+  const { mutate } = useCreateApp({
+    onSuccess: () => {
+      void query.refetch();
+    },
+  });
+
+  const execute = useCallback(() => {
+    // eslint-disable-next-line no-alert -- hello
+    const res = prompt('name?');
+    if (!res) return;
+    mutate({
+      name: res,
+    });
+  }, [mutate]);
 
   return (
     <div>
-      {Boolean(query.isLoading) && <p>Loading...</p>}
-      {Boolean(query.data?.count) && <p>You currently have {} apps.</p>}
+      <div className='flex justify-between items-center mb-2'>
+        <h1>Welcome to your dashboard!</h1>
+        <Button onClick={execute}>Create a new app</Button>
+      </div>
+      <div>
+        <h2 className='text-xl mb-4'>Your apps</h2>
+        {Boolean(query.data?.data?.length) && (
+          <div className='flex gap-4'>
+            {query.data?.data?.map((app) => (
+              <div
+                className='flex flex-col py-3 px-4 rounded-md border border-gray-200 gap-3'
+                key={app.id}
+              >
+                <h3>App name: {app.name}</h3>
+                <Button>Manage</Button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
