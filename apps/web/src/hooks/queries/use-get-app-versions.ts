b62b59bca761d1aa5375b3eb5a7c3285901a5b47
Atomic71 <tomicandrej71@gmail.com>
2023-11-28 19:37:44 +0100

    feat: add fetching of app versions

---
+++ apps/web/src/hooks/queries/use-get-app-versions.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import type { Tables } from 'db';
+import { browserClient } from '../../db/supabase';
+import { keys } from './keys';
+
+//eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- inference is better
+export const useGetAppVersions = (id: Tables<'apps'>['id']) => {
+  const query = useQuery({
+    queryKey: [keys.app_versions, id],
+    queryFn: async () => {
+      const data = await browserClient
+        .from('app_versions')
+        .select('*', { count: 'exact' })
+        .eq('app', id)
+        .order('updated_at', { ascending: false });
+      return data;
+    },
+    staleTime: 1000 * 5 * 60,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+
+  return query;
+};
