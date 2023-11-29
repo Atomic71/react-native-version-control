418194e1356a4c9b63d9cf9f2129c332ce01e7f5
Atomic71 <tomicandrej71@gmail.com>
2023-11-29 09:04:28 +0100

    chore: utilize new modal context

--- apps/web/app/dashboard/apps/[id]/page.tsx
+++ apps/web/app/dashboard/apps/[id]/page.tsx
@@ -3,19 +3,20 @@
 import { Button } from '@supabase/ui';
 import { useQueryClient } from '@tanstack/react-query';
 import { redirect } from 'next/navigation';
-import { useRef } from 'react';
-import { useGetUserApp } from '../../../../src/hooks/queries/use-get-user-app';
+import AppVersionForm from '../../../../src/components/app-version-form';
+import AppVersionsOverview from '../../../../src/components/app-versions-overview';
+import { useDialogContext } from '../../../../src/context/ctx-dialog';
+import { useCreateVersion } from '../../../../src/hooks/mutations/use-create-app-version';
 import { useDeleteApp } from '../../../../src/hooks/mutations/use-delete-app';
 import { keys } from '../../../../src/hooks/queries/keys';
-import AppVersionForm from '../../../../src/components/app-version-form';
-import { useCreateVersion } from '../../../../src/hooks/mutations/use-create-app-version';
+import { useGetUserApp } from '../../../../src/hooks/queries/use-get-user-app';
 
 export default function Page({
   params,
 }: {
   params: { id: string };
 }): JSX.Element {
-  const dialogRef = useRef<HTMLDialogElement>(null);
+  const { setDialogClosed, setDialogOpen } = useDialogContext();
   const query = useGetUserApp(params.id);
   const queryClient = useQueryClient();
   const deleteApp = useDeleteApp({
@@ -28,8 +29,9 @@ export default function Page({
 
   const createVersion = useCreateVersion({
     onSuccess: () => {
+      setDialogClosed();
       void queryClient.invalidateQueries({
-        queryKey: [keys.apps],
+        queryKey: [keys.apps, params.id],
       });
     },
   });
@@ -56,38 +58,27 @@ export default function Page({
           Delete
         </Button>
       </div>
+      <AppVersionsOverview id={app.id} />
       <Button
         onClick={() => {
-          dialogRef.current?.showModal();
+          setDialogOpen({
+            title: 'Create new version',
+            el: (
+              <AppVersionForm
+                onSubmit={({ name, appOs }) => {
+                  createVersion.mutate({
+                    appId: app.id,
+                    name,
+                    os: appOs,
+                  });
+                }}
+              />
+            ),
+          });
         }}
       >
         Create new version
       </Button>
-      <dialog
-        className='w-1/2 h-1/2 p-4'
-        ref={dialogRef}
-      >
-        <div className='flex justify-between'>
-          <h3>Create new version</h3>
-          <Button
-            onClick={() => {
-              dialogRef.current?.close();
-            }}
-            type='text'
-          >
-            Close
-          </Button>
-        </div>
-        <AppVersionForm
-          onSubmit={({ name, appOs }) => {
-            createVersion.mutate({
-              appId: app.id,
-              name,
-              os: appOs,
-            });
-          }}
-        />
-      </dialog>
     </div>
   );
 }
