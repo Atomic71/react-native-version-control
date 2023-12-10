56ce77163fcb4016f6c8446e52c92de20c397a5d
Atomic71 <tomicandrej71@gmail.com>
2023-12-11 00:37:56 +0100

    export types from db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,16 @@
 import type { Database } from './index.types';
-
 export { type Database } from './index.types';
+
 export type Tables<T extends keyof Database['public']['Tables']> =
   Database['public']['Tables'][T]['Row'];
 export type Enums<T extends keyof Database['public']['Enums']> =
   Database['public']['Enums'][T];
+
+export type AppVersion = Tables<'app_versions'>;
+
+type RelevantAppVersionInfo = Omit<AppVersion, 'created_at' | 'updated_at'>;
+export type CreateAppVersionPayload = Omit<RelevantAppVersionInfo, 'id'>;
+export type UpdateAppVersionPayload = RelevantAppVersionInfo;
+
+export type App = Tables<'apps'>;
+export type CreateAppPayload = Pick<App, 'name'>;
