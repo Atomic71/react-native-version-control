7b29c82161359ca27dcf363e32f4635a29eb4398
Atomic71 <tomicandrej71@gmail.com>
2023-12-11 00:50:28 +0100

    remove obsolete types

--- apps/web/src/hooks/mutations/use-create-app-version.ts
+++ apps/web/src/hooks/mutations/use-create-app-version.ts
@@ -1,13 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
-import type { Tables } from 'db';
+import type { CreateAppVersionPayload } from 'db';
 import { browserClient } from '../../db/supabase';
-
-type AppVersion = Tables<'app_versions'>;
-
-export type CreateAppVersionPayload = Pick<
-  AppVersion,
-  'app_os' | 'app' | 'is_blocked' | 'is_latest' | 'version_number'
->;
 
 //eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- inference is better
 export const useCreateVersion = ({ onSuccess }: { onSuccess: () => void }) => {
